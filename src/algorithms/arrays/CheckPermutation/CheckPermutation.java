31d9d5836fd90461e8981f074881db7d5eaf7731
Acinate <Acinate@hotmail.com>
2019-06-13 23:48:53 -0400

    Updated permutation algorithm to be more efficient with less code

--- src/algorithms/arrays/CheckPermutation/CheckPermutation.java
+++ src/algorithms/arrays/CheckPermutation/CheckPermutation.java
@@ -8,35 +8,21 @@ public class CheckPermutation {
     public String[][] testStrings = {testString1,testString2};
 
     public boolean isPermutation(String s1, String s2) {
-        // Check if the strings are the same length
-        if (s1.length() == s2.length()) {
-            // Keep count of each letter in each string
-            HashMap<Character, Integer> charCounter1 = new HashMap<>();
-            for(char c : s1.toCharArray()) {
-                if (!charCounter1.containsKey(c)) {
-                    charCounter1.put(c,1);
-                } else {
-                    charCounter1.put(c,charCounter1.get(c) + 1);
-                }
-            }
-            HashMap<Character, Integer> charCounter2 = new HashMap<>();
-            for(char c : s1.toCharArray()) {
-                if (!charCounter2.containsKey(c)) {
-                    charCounter2.put(c,1);
-                } else {
-                    charCounter2.put(c,charCounter2.get(c) + 1);
-                }
-            }
-            for (char c : charCounter1.keySet()) {
-                if (charCounter1.get(c).intValue() == charCounter2.get(c).intValue()) {
-                    // Do nothing, same amount of characters
-                } else {
-                    return false;
-                }
-            }
-            return true;
+        if (s1.length() != s2.length()) return false; // Permutations must be same length
+
+        int[] letters = new int[128]; // Assumption ASCII
+        for (int i = 0; i < s1.length(); i++) {
+            letters[s1.charAt(i)]++;
         }
-        return false;
+
+        for (int i = 0; i < s2.length(); i++) {
+            letters[s2.charAt(i)]--;
+            if (letters[s2.charAt(i)] < 0) {
+                return false;
+            }
+        }
+
+        return true; // letters has no neg values, and therefore no pos values either
     }
 
     public void runTests() {

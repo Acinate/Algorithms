0e17569a2a5937f3145aeb78296c2a30f2f8a081
Acinate <Acinate@hotmail.com>
2019-09-30 19:54:18 -0400

    Optimized RobotInGrid Solution using a HashMap

--- src/algorithms/dynamic_programming/RobotInGrid/Readme.md
+++ src/algorithms/dynamic_programming/RobotInGrid/Readme.md
@@ -22,3 +22,20 @@ spot adjacent to (r,c-1), which are spots (r-1,c-1) and (r,c-2). Observe that we
 
 So then, to find a path from the origin, we just work backwards like this. Starting from the last cell, we try to
 find a path to each of its adjacent cells.
+
+The solution is O(2^(r+c)), since each path has r+c steps and there are two choices we can make at each step.
+
+We should look for a faster way.
+
+Often, we can optimize exponential algorithms by finding duplicate work. What work are we repeating?
+
+If we walk through the algorithm, we'll see that we are visiting squares multiple times. In fact, we visit each
+square many, many times. After all, we have rc squares but we're doing O(2^(r+c)) work. If we were only visiting
+each square once, we would probably have an algorithm that was O(rc) (unless we were somehow doing a lot of work
+during each visit).
+
+How does our current algorithm work? To find a path to (r,c), we look for a path to an adjacent coordinate: (r-1,c
+) or (r,c-1). Of course, if one of those squares is off limits, we ignore it. Then, we look at their adjacent
+coordinates: (r-2,c), (r-1,c-1), (r-1,c-1), and (r,c-2). The spot (r-1,c-1) visited (r-1,c-1) so that we don't waste
+our time.
+
--- src/algorithms/dynamic_programming/RobotInGrid/RobotInGrid.java
+++ src/algorithms/dynamic_programming/RobotInGrid/RobotInGrid.java
@@ -2,31 +2,41 @@ package algorithms.dynamic_programming.RobotInGrid;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class RobotInGrid {
     public ArrayList<Point> getPath(boolean[][] maze) {
         if (maze == null || maze.length == 0) return null;
 
         ArrayList<Point> path = new ArrayList<>();
-        if (getPath(maze, maze.length - 1, maze[0].length - 1, path)) {
+        HashSet<Point> failedPoints = new HashSet<>();
+        if (getPath(maze, maze.length - 1, maze[0].length - 1, path, failedPoints)) {
             return path;
         }
 
         return null;
     }
 
-    private boolean getPath(boolean[][] maze, int row, int col, ArrayList<Point> path) {
+    private boolean getPath(boolean[][] maze, int row, int col, ArrayList<Point> path,
+                            HashSet<Point> failedPoints) {
         if (row < 0 || col < 0 || !maze[row][col]) return false;
 
         boolean isAtOrigin = (row == 0) && (col == 0);
 
-        if (isAtOrigin || getPath(maze, row - 1, col, path)
-        || getPath(maze, row, col - 1, path)) {
-            Point p = new Point(row, col);
+        Point p = new Point(row, col);
+
+        /* If we've already visited this cell, return */
+        if (failedPoints.contains(p)) {
+            return false;
+        }
+
+        if (isAtOrigin || getPath(maze, row - 1, col, path, failedPoints)
+        || getPath(maze, row, col - 1, path, failedPoints)) {
             path.add(p);
             return true;
         }
 
+        failedPoints.add(p); // Cache result
         return false;
     }
 }
